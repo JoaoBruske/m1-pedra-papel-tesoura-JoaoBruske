96a4b913d1d49e1f707ea3712311bf8dd6d50c47
João Bruske <joaobruske@yahoo.com.br>
2024-01-25 09:41:12 -0300

    Adição da função playRockPaper
    Criada a função playRockPaperScissor responsável por indicar qual jogador venceu ou se houve empate.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 /* Desenvolva seu código abaixo */
+
+function playRockPaperScissor(player1, player2) {
+    let vencedor;
+  
+    if (player1 === "Pedra" && player2 === "Tesoura") {
+      vencedor = "Jogador 1";
+    } else if (player1 === "Papel" && player2 === "Pedra") {
+      vencedor = "Jogador 1";
+    } else if (player1 === "Tesoura" && player2 === "Papel") {
+      vencedor = "Jogador 1";
+    } else if (player2 === "Pedra" && player1 === "Tesoura") {
+      vencedor = "Jogador 2";
+    } else if (player2 === "Tesoura" && player1 === "Papel") {
+      vencedor = "Jogador 2";
+    } else if (player2 === "Papel" && player1 === "Pedra") {
+      vencedor = "Jogador 2";
+    } else {
+      return "Empate!";
+    }
+  
+    return `${vencedor} venceu!`;
+  }
