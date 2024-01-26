46a9e2a35d68a4e9573e7e59b006708072f33634
João Bruske <joaobruske@yahoo.com.br>
2024-01-26 15:03:11 -0300

    Inserido operador lógico OU
    Adicionado o operador lógico OU para deixar o código mais legível.

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 /* Desenvolva seu código abaixo */
 
 function playRockPaperScissor(player1, player2) {
-    let vencedor;
-  
-    if (player1 === "Pedra" && player2 === "Tesoura") {
-      vencedor = "Jogador 1";
-    } else if (player1 === "Papel" && player2 === "Pedra") {
-      vencedor = "Jogador 1";
-    } else if (player1 === "Tesoura" && player2 === "Papel") {
-      vencedor = "Jogador 1";
-    } else if (player2 === "Pedra" && player1 === "Tesoura") {
-      vencedor = "Jogador 2";
-    } else if (player2 === "Tesoura" && player1 === "Papel") {
-      vencedor = "Jogador 2";
-    } else if (player2 === "Papel" && player1 === "Pedra") {
-      vencedor = "Jogador 2";
-    } else {
-      return "Empate!";
-    }
-  
-    return `${vencedor} venceu!`;
+  if (
+    (player1 === "Pedra" && player2 === "Tesoura") ||
+    (player1 === "Papel" && player2 === "Pedra") ||
+    (player1 === "Tesoura" && player2 === "Papel")
+  ) {
+    return "Jogador 1 venceu!";
+  } else if (
+    (player2 === "Pedra" && player1 === "Tesoura") ||
+    (player2 === "Tesoura" && player1 === "Papel") ||
+    (player2 === "Papel" && player1 === "Pedra")
+  ) {
+    return "Jogador 2 venceu!";
+  } else {
+    return "Empate!";
   }
+}
